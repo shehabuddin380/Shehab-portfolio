2635f47b20607ba1e6393614fb112b0ed2e9c93f
Shihab Ahmed <shehabuddin380@gmail.com>
2026-06-14 23:30:05 +0600

    add a app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Blog from "./components/Blog"
+import Blog from "./components/Blog";
+import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 
 import Footer from "./components/Footer";
@@ -24,6 +25,7 @@ function App() {
       <Skills darkMode={darkMode} />
       <Projects darkMode={darkMode} />
       < Blog darkMode={darkMode} />
+      < Testimonials darkMode={darkMode} />
       <Contact darkMode={darkMode} />
       <Footer darkMode={darkMode} />
     </div>
