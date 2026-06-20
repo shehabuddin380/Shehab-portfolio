c3012275c4a8d8192d460a5814b0349aa9cfadc5
Shihab Ahmed <shehabuddin380@gmail.com>
2026-06-20 14:33:35 +0600

    fix :: protpolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import ContactPage from "./components/ContactPage";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
